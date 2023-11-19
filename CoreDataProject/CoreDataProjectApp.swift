75832c9f7a3da9188a96d8fe20dbd183f54d4b26
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2023-11-19 19:07:23 +0600

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by A.f. Adib on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
